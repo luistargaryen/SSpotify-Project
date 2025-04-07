766f131b2a636cef5c607e6834f88162540c07eb
luistargaryen <luis.30833554@uru.edu>
2025-04-07 12:52:05 -0400

    Estructura general de la pagina register pero sin logica

--- SSpotify/src/app/pages/register/register.page.ts
+++ SSpotify/src/app/pages/register/register.page.ts
@@ -1,20 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonicModule, CommonModule, FormsModule, RouterLink ]
 })
 export class RegisterPage implements OnInit {
+  public username = '';
+  public password = '';
+  public email = '';
+  public rol = '';
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+    if (localStorage.getItem('token')) this.router.navigate(['/tabs']);
+    return;
   }
 
+  async register() {
+    // try {
+    //   console.log(this.username, this.password, this.email, this.rol);
+    //   if (
+    //     this.username === '' ||
+    //     this.password === '' ||
+    //     this.email === '' ||
+    //     this.rol === ''
+    //   ) {
+    //     alert('Porfavor completa todos los campos');
+    //     return;
+    //   }
+
+    //   const result = (await this.loginService.sendRegisterRequest(
+    //     username: this.username,
+    //     password: this.password,
+    //     email: this.email,
+    //     rol: this.rol
+    //   )) as any;
+
+    //   console.log(result);
+
+    //   if (!result || result?.error) {
+    //     throw new Error(`Ocurrio un error al registrarse. ${result?.error}`);
+    //   }
+
+    //     alert('Vaya a su correo para activar la cuenta')
+    //   } catch (error: any) {
+    //     console.error(`Error al registrarse ${error.error.error}`);
+    //     alert(`Error al registrarse ${error.error.error}`);
+    //   } finally {
+    //   }
+    // }
+  }
 }
