c0cb7f213c9d450dccc5ecdf2604412f94feafbb
luistargaryen <luis.30833554@uru.edu>
2025-04-07 15:26:46 -0400

    Login

--- SSpotify/src/app/pages/login/login.page.ts
+++ SSpotify/src/app/pages/login/login.page.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+import { LoaderService } from 'src/app/services/loader.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
+  username: string = '';
+  password: string = '';
 
-  constructor() { }
+  loginService = inject(LoginService);
+  loaderService = inject(LoaderService);
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.loaderService.hide();
+    if (localStorage.getItem('token')) this.router.navigate(['/tabs']);
+    return;
   }
 
+  async login() {
+    try {
+      this.loaderService.show();
+      alert('Iniciando sesion')
+      const result = (await this.loginService.sendLoginRequest(
+        // this.username,
+        // this.password
+      )) as any;
+      localStorage.setItem('token', result.token);
+      this.router.navigate(['/tabs']);
+      alert('Sesion iniciada')
+      return;
+    } catch (error) {
+      console.error('Error al iniciar sesion', error);
+      alert('Error al iniciar sesion')
+    } finally {
+      this.loaderService.hide();
+    }
+  }
 }
--- SSpotify/src/app/services/login.service.ts
+++ SSpotify/src/app/services/login.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   constructor() { }
+
+  sendLoginRequest(){}
+
+  sendRegisterRequest(){}
+
+  obtainDataUser(){}
+
+  updateDataUser(){}
+
+  deleteAccount(){}
+
 }
