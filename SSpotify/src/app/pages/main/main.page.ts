3b60759294b56569331e97a7f601960885f4ba0e
luistargaryen <luis.30833554@uru.edu>
2025-04-07 12:32:27 -0400

    Estructura General de la Pagina Main

--- SSpotify/src/app/pages/main/main.page.ts
+++ SSpotify/src/app/pages/main/main.page.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink, Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonicModule, RouterLink]
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    if (localStorage.getItem('token')) {
+      this.router.navigate(['/tabs']);
+      return;
+    }
   }
 
 }
