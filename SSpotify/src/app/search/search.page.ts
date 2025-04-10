41c88710a8ae81418c24dee9455a35e26d93f7a8
luistargaryen <luis.30833554@uru.edu>
2025-04-09 20:25:01 -0400

    Buscar

--- SSpotify/src/app/search/search.page.ts
+++ SSpotify/src/app/search/search.page.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SongSearchService } from '../services/song-search.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: 'search.page.html',
   styleUrls: ['search.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,]
+  imports: [IonicModule, CommonModule, FormsModule],
+  standalone: true
 })
-export class SearchPage {
+export class SearchPage implements OnInit {
+  songSearchService = inject(SongSearchService);
+  public genres = Array<string>();
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  colors = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger'];
+
+  async ngOnInit() {
+    try {
+      const response = await this.songSearchService.getGenres();
+      console.log(response);
+      this.genres = response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async search(name: string) {
+    this.router.navigate(['/searchSong', name]);
+  }
+
+  handleInput(event: any) {
+    const query = event.target.value.toLowerCase();
+    this.search(query);
+  }
+
+  getRandomColor(genre: string) {
+    const genreIndex = this.genres.indexOf(genre);
+    if (genreIndex === -1) {
+      return 'default'; // Retorna un color predeterminado si el género no se encuentra
+    }
+    const colorIndex = genreIndex % this.colors.length;
+    return this.colors[colorIndex];
+  }
+
+  redirectToGenre(genre: string) {
+    this.router.navigate(['/songGenre', genre]);
+  }
 }
